582a48aa9e5a25a157535100a2fc18efbb662674
Andriy Dmytruk <andriy.dmytruk@oracle.com>
2024-11-15 09:59:19 -0500

    Update docs

--- test-suite-groovy/src/test/groovy/io/micronaut/docs/ioc/mappers/MappersSpec.groovy
+++ test-suite-groovy/src/test/groovy/io/micronaut/docs/ioc/mappers/MappersSpec.groovy
@@ -28,4 +28,69 @@ class MappersSpec extends Specification {
         productDTO.distributor == "Great Product Company"
         // end::mappers[]
     }
+
+    void testMerging() {
+        // tag::merge[]
+        given:
+        ChristmasMappers mappers = context.getBean(ChristmasMappers.class)
+
+        when:
+        ChristmasPresent result = mappers.merge(
+                new PresentPackaging(1f, "red"),
+                new Present(10f, "teddy bear")
+        )
+
+        then:
+        11 == result.weight
+        "red" == result.packagingColor
+        "teddy bear" == result.type
+        "Merry christmas" == result.greetingCard
+        // end::merge[]
+    }
+
+    void testAdditionalMappers() {
+        // tag::additional[]
+        given:
+        AdditionalMappers mappers = context.getBean(AdditionalMappers.class)
+
+        when:
+        ChristmasPresent result = mappers.merge(
+                new PresentPackaging(1f, "red"),
+                new Present(10f, "teddy bear"),
+                new AdditionalMappers.Card("Merry Christmas!")
+        )
+
+        then:
+        10 == result.weight
+        result.packagingColor == null
+        "teddy bear" == result.type
+        "Merry Christmas!" == result.greetingCard
+
+        when:
+        result = mappers.update(
+                result,
+                [
+                        "packagingColor": "blue",
+                        "christmasCard": "Merry Christmas!"
+                ]
+        )
+
+        then:
+        10 == result.weight;
+        result.packagingColor == "blue"
+        result.type == "teddy bear"
+        result.greetingCard == "Merry Christmas!!!"
+
+        when:
+        result = mappers.mergeWithMergeStrategy(
+                new PresentPackaging(1, "red"),
+                new Present(10, "teddy bear")
+        )
+
+        then:
+        result.weight == 11
+        result.packagingColor == "red"
+        result.type == "teddy bear"
+        // end::additional[]
+    }
 }
